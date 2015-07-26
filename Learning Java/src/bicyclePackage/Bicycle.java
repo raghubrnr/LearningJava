2d16d0007db6c78a6372bc393cacab114c73c114
Raghu <beeram.raghu@gmail.com>
2015-07-26 08:46:55 +0530

    BicycleDemo and Package Change - Understanding creation of objects from class

--- Learning Java/src/bicyclePackage/Bicycle.java
+++ Learning Java/src/bicyclePackage/Bicycle.java
@@ -1,5 +1,49 @@
-package MyPackage;
+package bicyclePackage;
 
 public class Bicycle {
 
+	private int gear;
+	private int speed;
+	private int cedace;
+	
+	void increasespeed(int value)
+	{
+		speed = speed + value;
+	}
+	
+	void decreasespeed(int value)
+	{
+		speed = speed - value;
+	}
+	
+	void changecedace(int value)
+	{
+		cedace = value;
+	}
+	
+	void changegear(int value)
+	{
+		setGear(value);
+	}
+	
+	void printstates()
+	{
+		System.out.println("Speed= "+speed+"\nCedace= "+cedace+"\nGear = "+getGear() );
+	}
+
+	public int getGear() {
+		return gear;
+	}
+
+	public void setGear(int gear) {
+		this.gear = gear;
+	}
+
+	public int getCedace() {
+		return cedace;
+	}
+
+	public void setCedace(int cedace) {
+		this.cedace = cedace;
+	}
 }
---
+++ Learning Java/src/bicyclePackage/BicycleDemo.java
@@ -0,0 +1,20 @@
+package bicyclePackage;
+
+public class BicycleDemo {
+
+	public static void main(String[] args) {
+		Bicycle Atlas = new Bicycle();
+		Bicycle Hero = new Bicycle();
+		
+		Atlas.setCedace(1);
+		Atlas.setGear(2);
+		Atlas.increasespeed(20);
+		Atlas.decreasespeed(30);
+		Atlas.printstates();
+		
+		Hero.setGear(5);
+		Hero.setCedace(20);
+		
+	}
+
+}
